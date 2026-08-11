eb7a5d41c47b34dda58f4d7c3583a412071007b2
Eathen <wengjingkai2001@gmail.com>
2026-08-11 10:56:21 +0800

    feat: add ja to SUPPORTED languages

--- js/i18n.js
+++ js/i18n.js
@@ -7,7 +7,7 @@
 (function() {
   'use strict';
   const STORAGE_KEY = 'huanan_lang';
-  const SUPPORTED = ['zh', 'en', 'th'];
+  const SUPPORTED = ['zh', 'en', 'th', 'ja'];
   const DEFAULT_LANG = 'zh';
   let currentLang = DEFAULT_LANG;
   let translations = {};
